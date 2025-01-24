cf6fd6634aa443525c5492e4e966bb1850b4b4bb
Gleb Fedchun <gleb.fedchun@gmail.com>
2025-01-24 09:34:31 +0300

    Hello world

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module gitActions
+module Ad
 
 go 1.23.1
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
-package gitActions
+package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(hello())
+}
+
+func hello() string {
+	return "Hello go"
 }
